7feeb51b259c023675ff66b8e9df1be1f8df025f
Python_coder_K <turntabroz@gmail.com>
2024-09-21 20:50:54 +0900

    python_test

--- 0921_1.py
+++ 0921_1.py
@@ -1,4 +1,15 @@
+n, m = map(int, input().split())
+p = int(input())
+l = list(map(int, input().split()))
+l2 = []
+for i in range(len(l)):
+    if l[i] % n == 0:
+        l2.append(l[i])
 
-for i in range(3):
-    print("Hello World!")
-
+min_l = max(l2)
+for i in range(m):
+    x = int(input())
+    if abs(x - l2[i]) < min_l:
+        min_l = l2[i]
+    print(min_l)
+    min_l = max(l2)
