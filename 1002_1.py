e2ba893db719dbb79b00acaf6b4a8ad9b37cbfc0
Python_coder_K <turntabroz@gmail.com>
2024-10-02 06:36:31 +0900

    1002_2

--- 1002_1.py
+++ 1002_1.py
@@ -2,7 +2,5 @@ def check_job(student, teacher):
     print(f"teacher: {teacher}")
     print(f"student: {student}")
 
-# 以下にコードを記述
-
-
-check_job("kirishima", "nekosense")
+# 以下のコードを修正
+check_job(teacher="nekosense", student="kirishima")
