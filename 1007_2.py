15c5943c9b5a0e90e765d075a5ec073351b0fa5c
Python_coder_K <turntabroz@gmail.com>
2024-10-07 06:26:24 +0900

    function2

--- 1007_2.py
+++ 1007_2.py
@@ -1,9 +1,9 @@
-# 掛け算関数を呼び出してみよう
+# 掛け算関数を作成してみよう
 
 def multiply(x, y):
+    # この下に処理を記述する
     return x * y
 
-
-# この下に関数呼び出しを記述する
-answer = multiply(3, 4)
-print(answer)
+print(multiply(3, 4))
+print(multiply(5, 7))
+print(multiply(12, 34))
