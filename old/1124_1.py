d7067ebc6be9c76a417ef6e1c8ea3cd1b66f1703
Python_coder_K <turntabroz@gmail.com>
2024-11-24 14:34:12 +0900

    udemy

---
+++ old/1124_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+a = np.array([1, 2, 3])
+print(a)
+
+print(a[1])
+
+print(a.shape)
+
+print(a.ndim)
+
+print(a.dtype.name)
+
+print(a.size)
+
+print(np.arange(0, 30, 5))
+
+print(np.zeros((3, 4)))
