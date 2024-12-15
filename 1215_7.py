8c1bfe43671676ee4be99f3aec6499333c56ce4c
Python_coder_K <turntabroz@gmail.com>
2024-12-15 09:47:25 +0900

    atcoder

---
+++ 1215_7.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+x = int(n * 1.08)
+
+if x > 206: print(":(")
+elif x == 206: print("so-so")
+else: print("Yay!")
---
+++ 1215_9.py
@@ -0,0 +1,3 @@
+a, b = map(int, input().split())
+
+print("Yes" if 6 * a >= b and 1 * a <= b else "No")
